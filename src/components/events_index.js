dfa8677ca68f8cd80ffe2e431d19e1a4d58fff83
kuukazuki <16ikuta@seian.jp>
2020-07-17 16:17:24 +0900

    Failed to compile

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,29 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { increment, decrement } from '../actions'
-
+import { readEvents } from '../actions'
+import _ from 'lodash'
+import { Link } from 'react-router-dom'
 
 class EventsIndex extends Component {
   componentDidMount() {
-    console.log("hi!")
     this.props.readEvents()
   }
 
+  renderEvents() {
+    return _.map(this.props.events, event => (
+      <tr key={event.id}>
+        <td>{event.id}</td>
+        <td>{event.title}</td>
+        <td>{event.body}</td>
+      </tr>
+    ))
+  }
+
+
   render() {
-    const props = this.props
 
     console.log(this.state)
 
     return (
       <React.Fragment>
-        <div>value: {props.value}</div>
-        <button onClick={props.increment}>+1</button>
-        <button onClick={props.decrement}>-1</button>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+        <Link to="/events/new">New Events</Link>
       </React.Fragment>
     )
   }
 }
-const mapStateToProps = state => ({ value: state.count.value })
+const mapStateToProps = state => ({ events: state.events })
 
 const mapDispatchToProps = ({ readEvents })
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
@@ -9,11 +11,17 @@ import EventsIndex from './components/events_index';
 import * as serviceWorker from './serviceWorker';
 // import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+        {/*
+        <Route exact path="/events/new" component={EventsNew} /> */}
+        <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
